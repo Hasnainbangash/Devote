bc6cd196c104223df8ff4befc08a10440b6f87c4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-26 14:07:56 +0500

    SOme blur properties added to the list vie aand to the background image view of the project

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -134,6 +134,9 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 } //: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
                 
                 // MARK: - NEW TASK ITEM
                 
@@ -159,6 +162,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 0.8 : 0, opaque: false)
             )
             .background(
                 backgroundGradient
